8914930ee385ab7961facc81f09ac385ca54a59a
032-SakhaEStras. <sakhaall333@gmail.com>
2026-01-29 13:54:46 +0700

    Update SwitchCase.js

--- SwitchCase.js
+++ SwitchCase.js
@@ -2,17 +2,17 @@ let menuAplikasi = 3;
 
 switch (menuAplikasi) {
   case 1:
-    console.log("Anda memilih: Settings");
+    console.log("Kamu memilih Settings");
     break;
   case 2:
-    console.log("Anda memilih: Start");
+    console.log("Kamu memilih Start");
     break;
   case 3:
-    console.log("Anda memilih: Profile");
+    console.log("Kamu memilih Profile");
     break;
   case 4:
-    console.log("Anda memilih: Keluar");
+    console.log("Kamu memilih Keluar");
     break;
   default:
-    console.log("Pilihan tidak valid");
+    console.log("Gak ada pilihan selain itu");
 }
